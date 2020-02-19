cd79ce548264cae22656644c51295022e9aa4237
Dot.Who <35942318+DotWho@users.noreply.github.com>
2020-02-19 13:52:18 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,7 +7,11 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
-#取消bootstrap为默认主题
+#修改机器名称
+sed -i 's/OpenWrt/DotWho v1.0/g' package/base-files/files/bin/config_generate
+#修改默认主题
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+#去除默认bootstrap主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
