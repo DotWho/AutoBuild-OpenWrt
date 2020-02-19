f00204f65690d7c7fd67142bb3f4f02da867c358
Dot.Who <35942318+DotWho@users.noreply.github.com>
2020-02-19 14:01:24 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,10 +8,10 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
 #修改机器名称
-sed -i 's/OpenWrt/DotWho v1.0/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/DotWho v1.0/g' openwrt/package/base-files/files/bin/config_generate
 #修改默认主题
-sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
 #去除默认bootstrap主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' openwrt/feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" openwrt/package/lean/default-settings/files/zzz-default-settings
